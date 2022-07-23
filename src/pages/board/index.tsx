4fd1ae196abd6df57ae2a93be8b34f1035861ee1
juliano340 <juliano340@gmail.com>
2022-07-23 12:37:47 -0300

    "Criação da tela board"

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,64 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+import { FiClock, FiEdit2, FiPlus, FiTrash } from "react-icons/fi";
+import { FiCalendar } from "react-icons/fi";
+
+export default function Board() {
+    return (
+        <>
+        <Head>
+            <title>Minhas tarefas - Board</title>
+        </Head>
+        <main className={styles.container}>
+            <form>
+                <input 
+                    type="text" 
+                    placeholder='Digite sua tarefa...'
+                />
+
+                <button type="submit">
+                    <FiPlus size={25} color="#17181f"/>
+                </button>
+            </form>
+
+            <h1>Você tem 2 tarefas!</h1>
+
+            <section>
+                <article className={styles.taskList}>
+                    <p>Aprender NextJS</p>
+
+                    <div className={styles.actions}>
+                        <div>
+                            <div>
+                                <FiCalendar size={20} color="#ffb800" />
+                                <time>23 Julho 2022</time>
+                                
+                            </div>
+                            <button>
+                                <FiEdit2 size={20} color="#FFF"/>
+                                <span>Editar</span>
+                            </button>
+                        </div>
+
+                   
+
+                        <button>
+                            <FiTrash size={20} color="#FF3636" />
+                            <span>Excluir</span>
+                        </button>
+                    </div>
+                </article>
+            </section>
+        </main>
+
+        <div className={styles.vipContainer}>
+            <h3>Obrigado por apoiar este projeto</h3>
+            <div>
+                <FiClock size={28} color="#FFF"/>
+                <time>Última doação realizada a 3 dias.</time>
+            </div>
+        </div>
+        
+        </>
+    )
+}
